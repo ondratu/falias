2969d39b69daf39539d0cdf9ab7c8779aa81e931
Ondrej Tuma <mcbig@zeropage.cz>
2015-05-30 13:06:59 +0200

    ninu function and Size and Paths parsing object

--- falias/util.py
+++ falias/util.py
@@ -4,6 +4,11 @@ def uni(text):
         return unicode(text, encoding = 'utf-8')
     return unicode(text)
 
+def nuni(val):
+    if val is None:
+        return None
+    return uni(val)
+
 def nint(val):
     if val is None:
         return None
@@ -27,3 +32,29 @@ class Object(object):
     """ Simple object with __contains__ method, so 'prop' in obj will work """
     def __contains__(self, key):
         return self.__dict__.__contains__(key)
+
+
+class Size(object):
+    """ Simple size object, which store size in width and height variable.
+        Size must be set in text as WIDTHxHEIGHT
+    """
+    def __init__(self, text):
+        self.width, self.height = tuple(int(x) for x in text.split('x'))
+
+    def __str__(self):
+        return "%dx%d" % (self.width, self.height)
+
+    def get(self):
+        return (self.width, self.height)
+
+
+class Paths(tuple):
+    def __init__(self, value):
+        if value:
+            super(tuple, self).__init__(uni(it.strip()) for it in uni(value).split(':'))
+        else:
+            super(tuple, self)
+
+
+    def __str__(self):
+        return ':'.join(self)
